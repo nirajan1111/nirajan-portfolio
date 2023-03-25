6c1984a0de190c609085cad2c826ff2f8da0de86
nirajan1111 <110184689+nirajan1111@users.noreply.github.com>
2023-03-26 00:35:51 +0545

    my photo added after thinking for 2 minutes

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,13 @@
 import "../styles.css";
 import "./about.css";
+import nirajan from "./images/nirajan-portfolio.png";
 export default function About() {
   return (
     <section id="about">
       <div className="wrapper">
         <div className="profile-card js-profile-card container">
           <div className="profile-card__img">
-            <img src="images/img-profile-dp.png" alt="profile card" />
+            <img src={nirajan} alt="profile card" />
           </div>
 
           <div className="profile-card__cnt js-profile-cnt">
